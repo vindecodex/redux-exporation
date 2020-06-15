4b5c2f350609ce1bf9a31b45df2aa56e46f18c2e
Vincent Villaluna <villalunavincent@gmail.com>
2020-06-15 09:54:27 +0800

    fixing input title instead of description

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ const unsubscribe = store.subscribe(() => {
 	console.log("Update!!!", store.getState());
 })
 
-store.dispatch(actions.todoAdded({ description: 'study react' }));
-store.dispatch(actions.todoAdded({ description: 'study redux' }));
-store.dispatch(actions.todoAdded({ description: 'study next' }));
-store.dispatch(actions.todoAdded({ description: 'study anyhting' }));
+store.dispatch(actions.todoAdded({ title: 'study react' }));
+store.dispatch(actions.todoAdded({ title: 'study redux' }));
+store.dispatch(actions.todoAdded({ title: 'study next' }));
+store.dispatch(actions.todoAdded({ title: 'study anyhting' }));
 store.dispatch(actions.todoDone({ id: 2 }));
 store.dispatch(actions.todoRemoved({ id: 1 }));
