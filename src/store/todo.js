cd62642f94773af720da905d8746608edc627612
Vincent Villaluna <villalunavincent@gmail.com>
2020-06-15 09:21:47 +0800

    implemented the createReducer from reduxjs-toolkit

--- src/store/todo.js
+++ src/store/todo.js
@@ -1,4 +1,4 @@
-import { createAction } from '@reduxjs/toolkit';
+import { createAction, createReducer } from '@reduxjs/toolkit';
 
 // ACTION CREATORS
 export const todoAdded = createAction('todoAdded');
@@ -9,24 +9,22 @@ export const todoDone = createAction('todoDone');
 // REDUCER
 let id = 0;
 const initialState = [];
-export default function reducer(state = initialState,action) {
-	switch(action.type) {
-		case todoAdded.type:
-			return [
-				...state,
-				{
-					id: ++id,
-					title: action.payload.title,
-					status: true
-				}
-			];
-		case todoRemoved.type:
-			return state.filter(todo => todo.id !== action.payload.id);
-		case todoDone.type:
-			return state.map(todo => 
-				todo.id !== action.payload.id ? todo : {...todo, status: false}
-			);
-		default:
-			return state;
+// createReducer accepts two arguments 1: initial state 2: object
+// key is the action creator type with a value of function the will manipulate the state
+export default createReducer(initialState, {
+	[todoAdded.type]: function(state, action) {
+		state.push({
+			id: ++id,
+			title: action.payload.title,
+			status: true
+		})
+	},
+	[todoRemoved.type]: function(state, action) {
+		const stateIndex = state.findIndex(todo => todo.id === action.payload.id)
+		state.splice(stateIndex,1)
+	},
+	[todoDone.type]: function(state, action) {
+		const stateIndex = state.findIndex(todo => todo.id === action.payload.id)
+		state[stateIndex].status = false
 	}
-}
+})
