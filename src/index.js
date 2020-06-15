6243e789a91a6f58c697f89004a961fa96562e57
Vincent Villaluna <villalunavincent@gmail.com>
2020-06-15 16:57:44 +0800

    sample dispatching a function

--- src/index.js
+++ src/index.js
@@ -8,11 +8,18 @@ const unsubscribe = store.subscribe(() => {
 	console.log("Update!!!", store.getState());
 })
 
-store.dispatch(todoAdded({ title: 'study react' }));
-store.dispatch(todoAdded({ title: 'study redux' }));
-store.dispatch(todoAdded({ title: 'study next' }));
-store.dispatch(todoAdded({ title: 'study anyhting' }));
-store.dispatch(userAdded({ name: 'vincent' }));
-store.dispatch(todoDone({ id: 2 }));
-store.dispatch(todoRemoved({ id: 1 }));
-console.log(todoDoneSelector(store.getState()));
+// store.dispatch(todoAdded({ title: 'study react' }));
+// store.dispatch(todoAdded({ title: 'study redux' }));
+// store.dispatch(todoAdded({ title: 'study next' }));
+// store.dispatch(todoAdded({ title: 'study anyhting' }));
+// store.dispatch(userAdded({ name: 'vincent' }));
+// store.dispatch(todoDone({ id: 2 }));
+// store.dispatch(todoRemoved({ id: 1 }));
+// console.log(todoDoneSelector(store.getState()));
+store.dispatch((getState, dispatch) => {
+	// example we have API CALL
+	let apiResp = { title: 'My-Thunk' };
+	// After we get the response we need to dispatch it
+	dispatch(todoAdded(apiResp));
+	});
+
