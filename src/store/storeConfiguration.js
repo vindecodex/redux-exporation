b7d4dd7fe12e8efa20631d43c6022db9b66e96fd
Vincent Villaluna <villalunavincent@gmail.com>
2020-06-15 17:12:46 +0800

    using default-thunk-middleware from reduxjs/toolkit

--- src/store/storeConfiguration.js
+++ src/store/storeConfiguration.js
@@ -1,15 +1,15 @@
 import { createStore } from 'redux';
 import reducer from './reducer';
 import logger from './middleware/logger';
-import myThunk from './middleware/myThunk';
+// import myThunk from './middleware/myThunk';
 
 // used in debugging
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 
 export default function() {return configureStore({ 
 	reducer,
 	middleware: [
+		...getDefaultMiddleware(),
 		logger('CONSOLE'),
-		myThunk
 	]
 });}
