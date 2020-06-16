35393b15ad470ac374f5b40b8d1e6825d2eb6e80
Vincent Villaluna <villalunavincent@gmail.com>
2020-06-16 10:29:30 +0800

    explained configureStore and getDefaultMiddleware

--- src/store/storeConfiguration.js
+++ src/store/storeConfiguration.js
@@ -7,10 +7,11 @@ import api from './middleware/api';
 // used in debugging
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 
+// configureStore is an improve method for createStore => it automatically configure the debugging tools.
 export default function() {return configureStore({ 
 	reducer,
 	middleware: [
-		...getDefaultMiddleware(),
+		...getDefaultMiddleware(), // it includes the thunk middleware already because of this
 		logger('CONSOLE'),
 		api
 	]
