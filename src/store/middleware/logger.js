ef7a1bff674ac7e0101e599b13fc5c04da2e24d1
Vincent Villaluna <villalunavincent@gmail.com>
2020-06-16 12:23:03 +0800

    logging payload every dispatch

--- src/store/middleware/logger.js
+++ src/store/middleware/logger.js
@@ -2,7 +2,8 @@ const logger = param => store => next => action => {
 	// console.log('store', store);
 	// console.log('next', next);
 	// console.log('action', action);
-	console.log('Param', param);
+	// console.log('param', param);
+	console.log(action.payload);
 	next(action);
 }
 
